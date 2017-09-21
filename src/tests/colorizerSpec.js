acdff46efaf8163054e5dd1e9ab9953968520562
marten silbiger <marten@metal-heart.org>
2017-09-21 11:45:51 -0700

    Much happier with this implementation

--- src/tests/colorizerSpec.js
+++ src/tests/colorizerSpec.js
@@ -33,69 +33,79 @@ describe('restOfPhraseMatcher', () => {
 describe('phraseMatcher', () => {
   it('should create a stylesheet with my phrase', () => {
     const text = "one two three four".split(' ');
-    const id = "red";
+    const id = 4;
     const phrase = "two three";
-    const stylesheet = colorizer.phraseMatcher(text, id, phrase);
-    expect(stylesheet[1].start[0]).to.be.equal(id);
-    expect(stylesheet[2].end[0]).to.be.equal(id);
+    const stylesheet = colorizer.updateStylesWithPhrase(text, id, phrase);
+    expect(stylesheet[1][0]).to.be.equal(id);
+    expect(stylesheet[2][0]).to.be.equal(id);
   });
   it('should find two phrases', () => {
     const text = "one two three four one two three four".split(' ');
     const id = "red";
     const phrase = "two three";
-    const stylesheet = colorizer.phraseMatcher(text, id, phrase);
+    const stylesheet = colorizer.updateStylesWithPhrase(text, id, phrase);
     console.log(stylesheet);
-    expect(stylesheet[1].start[0]).to.be.equal(id);
-    expect(stylesheet[2].end[0]).to.be.equal(id);
-    expect(stylesheet[5].start[0]).to.be.equal(id);
-    expect(stylesheet[6].end[0]).to.be.equal(id);
+    expect(stylesheet[1][0]).to.be.equal(id);
+    expect(stylesheet[2][0]).to.be.equal(id);
+    expect(stylesheet[5][0]).to.be.equal(id);
+    expect(stylesheet[6][0]).to.be.equal(id);
   });
+  it('should handle overlapping phrases and passing by reference', () => {
+    const text = "one two three four".split(' ');
+    const stylesheet = [];
+    colorizer.updateStylesWithPhrase(text, 'first', 'two three', stylesheet);
+    colorizer.updateStylesWithPhrase(text, 'second', 'three four', stylesheet);
+    expect(stylesheet[1][0]).to.be.equal('first');
+    expect(stylesheet[2][0]).to.be.equal('first');
+    expect(stylesheet[2][1]).to.be.equal('second');
+    expect(stylesheet[3][0]).to.be.equal('second');
+  })
 });
 
 describe('applyStyles', () => {
   it('should work ok', () => {
     const text = "one two three four".split(' ');
-    const stylesheet = [
+    const styleMap = [
       null,
-      {start: [0]},
-      {end: [0]},
+      [0],
+      [0],
       null
     ];
     const styleGuide = [
       {style: 'red'}
     ];
-    const res = colorizer.applyStyles(text, stylesheet, styleGuide);
+    const res = colorizer.applyStyles(text, styleMap, styleGuide);
     console.log(res);
     expect(res[0]).to.be.equal('one');
     expect(res[1]).to.be.equal('<span className="red">two');
-    expect(res[2]).to.be.equal('three</span>');
-    expect(res[3]).to.be.equal('four');
+    expect(res[2]).to.be.equal('three');
+    expect(res[3]).to.be.equal('</span>four');
   });
 
   it('should handle overlapping styles, priority up front', () => {
     const text = "one two three four".split(' ');
-    const stylesheet = [
-      {start: [0]},
-      {start: [1], end: [0]},
-      {end: [1]}
+    const styleMap = [
+      [0],
+      [0, 1],
+      [1],
     ];
     const styleGuide = [
       {style: 'red'},
       {style: 'blue'}
     ];
-    const res = colorizer.applyStyles(text, stylesheet, styleGuide);
+    const res = colorizer.applyStyles(text, styleMap, styleGuide);
     console.log(res);
     expect(res[0]).to.be.equal('<span className="red">one');
-    expect(res[1]).to.be.equal('two</span><span className="blue">');
-    expect(res[2]).to.be.equal('three</span>');
-    expect(res[3]).to.be.equal('four');
+    expect(res[1]).to.be.equal('two');
+    expect(res[2]).to.be.equal('</span><span className="blue">three');
+    expect(res[3]).to.be.equal('</span>four');
   });
   it('should handle overlapping styles, priority in back', () => {
     const text = "one two three four".split(' ');
     const stylesheet = [
-      {start: [1]},
-      {start: [0], end: [1]},
-      {end: [0]}
+      [1],
+      [0, 1],
+      [0]
     ];
     const styleGuide = [
       {style: 'red'},
@@ -105,7 +115,23 @@ describe('applyStyles', () => {
     console.log(res);
     expect(res[0]).to.be.equal('<span className="blue">one');
     expect(res[1]).to.be.equal('</span><span className="red">two');
+    expect(res[2]).to.be.equal('three');
+    expect(res[3]).to.be.equal('</span>four');
+  });
+  it('should handle a phrase at the end of the block', () => {
+    const text = "one two three".split(' ');
+    const stylesheet = [
+      [0],
+      [0],
+      [0]
+    ];
+    const styleGuide = [
+      {style: 'red'},
+    ];
+    const res = colorizer.applyStyles(text, stylesheet, styleGuide);
+    console.log(res);
+    expect(res[0]).to.be.equal('<span className="red">one');
+    expect(res[1]).to.be.equal('two');
     expect(res[2]).to.be.equal('three</span>');
-    expect(res[3]).to.be.equal('four');
   });
 });
--- src/utils/colorizer.js
+++ src/utils/colorizer.js
@@ -14,98 +14,83 @@ const restOfPhraseMatcher = (text, idx, wordsInPhrase) => {
  * @param  {[type]} phrase [description]
  * @return {[type]}        [description]
  */
-const phraseMatcher = (text, id, phrase, stylesheet = []) => {
+const updateStylesWithPhrase = (text, id, phrase, styleMap = []) => {
   const wordsInPhrase = phrase.split(' ');
-  if (wordsInPhrase.length < 1) return stylesheet;
+  if (wordsInPhrase.length < 1) return styleMap;
 
   let cursor = text.indexOf(wordsInPhrase[0]);
   while (cursor >= 0) {
     if (restOfPhraseMatcher(text, cursor, wordsInPhrase)) {
-      // write to our "style sheet"
-      if (!stylesheet[cursor]) {
-        stylesheet[cursor] = {};
+      // @TODO es6 array functions here?
+      const endOfPhrase = cursor + wordsInPhrase.length;
+      for (let i = cursor; i < endOfPhrase; i++) {
+        if (!styleMap[i]) {
+          styleMap[i] = [];
+        }
+        styleMap[i].push(id);
       }
-      if (!stylesheet[cursor].start) {
-        stylesheet[cursor].start = [];
-      }
-      stylesheet[cursor].start.push(id);
-
-      const endOfPhrase = cursor + wordsInPhrase.length - 1;
-      if (!stylesheet[endOfPhrase]) {
-        stylesheet[endOfPhrase] = {};
-      }
-      if (!stylesheet[endOfPhrase].end) {
-        stylesheet[endOfPhrase].end = [];
-      }
-      stylesheet[endOfPhrase].end.push(id);
     }
     cursor = text.indexOf(wordsInPhrase[0], cursor + 1);
   }
-  return stylesheet;
+  return styleMap;
 };
 
-const getStylesheet = (text, styleGuide) => {
+const getStyleMap = (text, styleGuide) => {
   const textWords = text.split(' ');
-  let styles = [];
+  const styleMap = [];
   styleGuide.forEach(({ phrases }, idx) => {
     phrases.forEach((phrase) => {
-      styles = phraseMatcher(textWords, idx, phrase, styles);
+      updateStylesWithPhrase(textWords, idx, phrase, styleMap);
     });
   });
-  return styles;
+  return styleMap;
 };
 
-const applyStyles = (text, stylesheet, styleGuide) => {
+const applyStyles = (text, styleMap, styleGuide) => {
   let activeStyle = null;
 
-  let activeStyles = [];
-
   const result = [];
-  let output = '';
+  let tags = '';
   for (let i = 0; i < text.length; i++) {
-    output = text[i];
-    console.log('now on index', i, ' with styles:', stylesheet[i]);
-    if (stylesheet[i] && stylesheet[i].start) {
-      activeStyle = activeStyles[0]; // if it's not already.
-      activeStyles = activeStyles.concat(stylesheet[i].start).sort();
-      console.log('now activeStyles is:', activeStyles);
-      const useThisStyle = activeStyles[0];
-      // null, undefined
-      if (activeStyle === undefined) {
-        output = `<span className="${styleGuide[useThisStyle].style}">${output}`;
-      } else {
-        if (useThisStyle < activeStyle) {
-          // kill the old style and start applying the new
-          output = `</span><span className="${styleGuide[useThisStyle].style}">${output}`;
-        }
-      }
+    tags = '';
+    // console.log('considering:', i, activeStyle, styleMap[i]);
+    // check for ending styles first.
+    // apply a close span if:
+    // * there is an active style
+    // * the style for the word we're checking is empty, or different from
+    //   the active style.
+    if (activeStyle !== null &&
+      !(styleMap[i] && styleMap[i].length > 0 && activeStyle === styleMap[i][0])) {
+      tags += '</span>';
+      activeStyle = null;
     }
 
-    if (stylesheet[i] && stylesheet[i].end) {
-      activeStyle = activeStyles[0]; // if it's not already.
-
-      console.log('removing ', stylesheet[i].end, ' from ', activeStyles);
-      activeStyles = activeStyles.filter(x => !stylesheet[i].end.includes(x));
-      console.log('result: ', activeStyles, 'while activeStyle is:', activeStyle);
-
-      if (activeStyles.length > 0) {
-        const useThisStyle = activeStyles[0];
-        if (activeStyle !== activeStyles[0]) {
-          output = `${output}</span><span className="${styleGuide[useThisStyle].style}">`;
-        }
-      } else {
-        output = `${output}</span>`;
-      }
+    if (activeStyle === null && styleMap[i]) {
+      // assuming these are sorted with 0-index being the highest priority
+      activeStyle = styleMap[i][0];
+      tags = `${tags}<span className="${styleGuide[activeStyle].style}">`;
     }
 
-    result.push(output);
+    result.push(`${tags}${text[i]}`);
+  }
+  // close final span if text ends with a phrase
+  if (activeStyle !== null) {
+    result[result.length - 1] += '</span>';
   }
   return result;
 }
 
+const colorize = (text, styleGuide) => {
+  const paragraphs = text.split('\n');
+  let stylesheet;
+  paragraphs.map(paragraph => {
+    stylesheet = getStylesheet()
+  });
+}
+
 export default {
-  phraseMatcher,
+  updateStylesWithPhrase,
   restOfPhraseMatcher,
-  getStylesheet,
+  getStyleMap,
   applyStyles,
 };
